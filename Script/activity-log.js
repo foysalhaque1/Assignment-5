7c89419bd0fa72bb78fa9125ad8b7f7c7307a7c1
user <foysalhaque.mist.ipe@gmail.com>
2025-07-09 19:01:45 +0600

    update of button js

--- Script/activity-log.js
+++ Script/activity-log.js
@@ -1,6 +1,9 @@
 document.getElementById("clear-history").addEventListener("click",
     function(){
         document.getElementById("history-1").style.display = "none"
+        document.getElementById("history-2").style.display = "none"
+        document.getElementById("history-3").style.display = "none"
+        document.getElementById("history-4").style.display = "none"
 
 
 })
---
+++ Script/completed-btn-4.js
@@ -0,0 +1,33 @@
+document.getElementById("completed-btn-4").addEventListener("click",
+    function(event){
+        event.preventDefault();
+        const now = new Date()
+        
+        const totalTasks = document.getElementById("total-tasks").innerText;
+        console.log(totalTasks);
+        const convertedTotalTasks = parseInt(totalTasks);
+        this.disabled = true;
+
+        const completedTasks = document.getElementById("completed-tasks").innerText;
+        const convertedCompletedTasks = parseInt(completedTasks);
+        const remainingTotalTasks = convertedTotalTasks - 1;
+        const remainingTotalCompletedTasks = convertedCompletedTasks + 1;
+        document.getElementById("total-tasks").innerText = remainingTotalTasks;
+        document.getElementById("completed-tasks").innerText = remainingTotalCompletedTasks;
+        alert('Task-4 is completed')
+        const container = document.getElementById("activity-log");
+        const div = document.createElement("div");
+        const title = document.getElementById("title-4").innerText
+        console.log(title);
+        div.classList.add('bg-white','border','rounded-lg','m-[20px]','history');
+        div.id = 'history-4'
+        div.innerHTML = `
+        <p class="p-2"> You have Complete The Task <span class="text-blue-500"> ${title}</span> at ${now}</p>
+    
+        
+        
+        `
+        container.appendChild(div)
+       
+
+    })
